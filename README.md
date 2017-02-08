1f5bbfa98f96474797207435a2c8ed76cd7a1938
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-02-07 17:39:24 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#ng-book repo
+
+###This is a collection of apps that I built, with the code coming from ng-book by Ari Lerner
+
+-Each file is me following along with the book, copying the code from each example and creating the apps within the books. 
---
+++ hello-world-app/js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function MyController($scope){
+  $scope.clock = {
+    now: new Date()
+  }
+  function updateClock(){
+    $scope.clock.now = new Date();
+  }
+  setInterval(function(){
+    $scope.$apply(updateClock);
+  }, 1000);
+  updateClock();
+};
