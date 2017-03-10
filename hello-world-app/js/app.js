22ad457342f9f29fb894a8fb6e0843450c409f17
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-03-10 10:29:12 -0800

    scope part 1

--- hello-world-app/js/app.js
+++ hello-world-app/js/app.js
@@ -1,14 +1,24 @@
 'use strict';
 
-function MyController($scope){
-  $scope.clock = {
-    now: new Date()
-  }
-  function updateClock(){
-    $scope.clock.now = new Date();
-  }
-  setInterval(function(){
-    $scope.$apply(updateClock);
-  }, 1000);
-  updateClock();
-};
+// function MyController($scope){
+//   $scope.clock = {
+//     now: new Date()
+//   };
+//   function updateClock(){
+//     $scope.clock.now = new Date();
+//   }
+//   setInterval(function(){
+//     $scope.$apply(updateClock);
+//   }, 1000);
+//   updateClock();
+// };
+
+// angular.module('myApp', [])
+//   .run(function($rootScope){
+//     $rootScope.name = 'World';
+//   });
+
+angular.module('myApp', [])
+  .controller('MyController', function($scope){
+    $scope.name = 'Ari';
+  });
